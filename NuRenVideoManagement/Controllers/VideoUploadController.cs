2ab482bcf8658951ef65a5ace2f8ddf4d15755f8
Christopher Smith <csmith4work@gmail.com>
2019-06-07 12:20:28 -0500

    Boilerplate.

---
+++ NuRenVideoManagement/Controllers/VideoUploadController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NuRenVideoManagement.Controllers
+{
+    [Route("v1/api/[controller]")]
+    [ApiController]
+    public class VideoUploadController : ControllerBase
+    {
+
+    }
+}
