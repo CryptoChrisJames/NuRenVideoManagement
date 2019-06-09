a055a5d7f20cfb96838a2ecf9a8d4a8020fa19fb
Christopher Smith <csmith4work@gmail.com>
2019-06-08 22:30:51 -0500

    Setting up structure.

---
+++ NuRenVideoManagement/Extensions/AppicationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NuRenVideoManagement.Extensions
+{
+    public static class AppicationExtensions
+    {
+        public static IServiceCollection AddAppicationExtensions(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
